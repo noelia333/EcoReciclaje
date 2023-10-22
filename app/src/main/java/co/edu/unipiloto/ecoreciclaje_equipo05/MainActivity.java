6e3257389d2e4d7aa6496ad3882ab8b9743d3ffa
julian sanchez <juliansanchez623@gmail.com>
2023-10-22 11:25:22 -0500

    conexion vistas

--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/MainActivity.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/MainActivity.java
@@ -1,9 +1,15 @@
 package co.edu.unipiloto.ecoreciclaje_equipo05;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button iniciar;
 
 
     @Override
@@ -11,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        iniciar=findViewById(R.id.btn_iniciar);
+
+        Intent login=new Intent(getApplicationContext(), Inicio_sesion.class);
+
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(login);
+            }
+        });
 
     }
 }
