527223daeb536156b95a9b3ce2fc572c59500501
julian sanchez <juliansanchez623@gmail.com>
2023-10-24 08:48:21 -0500

    estadisticas2

---
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/pantalla_estadisticas.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.ecoreciclaje_equipo05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantalla_estadisticas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_estadisticas);
+    }
+}
