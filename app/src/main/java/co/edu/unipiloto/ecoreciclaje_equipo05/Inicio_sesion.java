6be9cd26aba75ec78e4455727660b12b5ab6ede1
julian sanchez <juliansanchez623@gmail.com>
2023-10-24 20:17:09 -0500

    conexion1

--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/Inicio_sesion.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/Inicio_sesion.java
@@ -5,11 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Inicio_sesion extends AppCompatActivity {
 
+    EditText ext_name, ext_password;
     TextView registrarte;
+    Button iniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,13 @@ public class Inicio_sesion extends AppCompatActivity {
         setContentView(R.layout.activity_inicio_sesion);
 
         registrarte=findViewById(R.id.txt_registrate);
+        iniciarSesion=findViewById(R.id.btn_iniciar_sesion);
+        ext_name=findViewById(R.id.ext_name);
+        ext_password=findViewById(R.id.ext_password);
+
 
         Intent regis=new Intent(getApplicationContext(), registro_usuario.class);
+        Intent logi=new Intent(getApplicationContext(), Perfil_usuario.class);
 
         registrarte.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +35,22 @@ public class Inicio_sesion extends AppCompatActivity {
                 startActivity(regis);
             }
         });
+
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameText=ext_name.getText().toString();
+                String passwordText=ext_password.getText().toString();
+                if (nameText.isEmpty() || passwordText.isEmpty()){
+                    Toast.makeText(getBaseContext(),"Todos los campos deben llenarse",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    startActivity(logi);
+                }
+            }
+        });
+
+
 
 
     }
