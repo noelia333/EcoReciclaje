a62cd59af544ecb893239a18144a1d44d6660a96
Cat3rine123 <caterine841210@gmail.com>
2023-10-26 00:14:10 -0500

    cambio

--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/registro_usuario.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/registro_usuario.java
@@ -3,12 +3,32 @@ package co.edu.unipiloto.ecoreciclaje_equipo05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class registro_usuario extends AppCompatActivity {
+    Button btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        btnRegistrarse=findViewById(R.id.btn_registrarse);
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registroUsuario=new Intent(getApplicationContext(),Inicio_sesion.class);
+
+                startActivity(registroUsuario);
+            }
+        });
+
+
+
     }
 }
