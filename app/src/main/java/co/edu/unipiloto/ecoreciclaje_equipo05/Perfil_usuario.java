afa734c87e6729bdcaca6f6e056623f12966aca5
Alexander <alepues1@gmail.com>
2023-10-27 18:12:56 -0500

    Creo Inten imv, tv material reciclable

--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/Perfil_usuario.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/Perfil_usuario.java
@@ -12,13 +12,21 @@ public class Perfil_usuario extends AppCompatActivity {
 
     ImageView imvConsejos;
 
+    ImageView imvMaterialReciclable;
+
     TextView tvConsejos;
+
+    TextView tvMaterialRecicable;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario);
 
+        // IMAGEN CONSEJOS
         imvConsejos=findViewById(R.id.imvConsejos);
         imvConsejos.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +37,7 @@ public class Perfil_usuario extends AppCompatActivity {
             }
         });
 
+        // TEXTO CONSEJOS
         tvConsejos=findViewById(R.id.tvConsejos);
         tvConsejos.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +47,28 @@ public class Perfil_usuario extends AppCompatActivity {
                 startActivity(Perfil_usuario);
             }
         });
+
+        // IMAGEN MATERIAL RECICLABLE
+
+        imvMaterialReciclable=findViewById(R.id.imvMaterialReciclable);
+        imvMaterialReciclable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Perfil_usuario=new Intent(getApplicationContext(),registro_materiales.class);
+
+                startActivity(Perfil_usuario);
+            }
+        });
+
+        // TEXTO MATERIAL RECICLABLE
+        tvMaterialRecicable=findViewById(R.id.tvMaterialRecicable);
+        tvMaterialRecicable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Perfil_usuario=new Intent(getApplicationContext(),registro_materiales.class);
+                startActivity(Perfil_usuario);
+            }
+        });
+
     }
 }
