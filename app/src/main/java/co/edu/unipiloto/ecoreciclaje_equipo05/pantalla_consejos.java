1f87f267291d7664c7299dadd7c43e9273d4361f
Alexander <alepues1@gmail.com>
2023-10-27 17:21:17 -0500

    Agrego intent retroceder

--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/pantalla_consejos.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/pantalla_consejos.java
@@ -2,13 +2,29 @@ package co.edu.unipiloto.ecoreciclaje_equipo05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class pantalla_consejos extends AppCompatActivity {
+
+    ImageView ivDevolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_consejos);
+
+        ivDevolver=findViewById(R.id.ivDevolver);
+        ivDevolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pantalla_consejos=new Intent(getApplicationContext(),Perfil_usuario.class);
+                startActivity(pantalla_consejos);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/registro_usuario.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/registro_usuario.java
@@ -2,6 +2,7 @@ package co.edu.unipiloto.ecoreciclaje_equipo05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
