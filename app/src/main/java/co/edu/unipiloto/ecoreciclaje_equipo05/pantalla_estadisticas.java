6e5442654d8f7d6e3cf6be820203e8283884cb8d
julian sanchez <juliansanchez623@gmail.com>
2023-10-28 18:40:51 -0500

    voler a perfil desde estadisticas

--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/pantalla_estadisticas.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/pantalla_estadisticas.java
@@ -2,13 +2,28 @@ package co.edu.unipiloto.ecoreciclaje_equipo05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class pantalla_estadisticas extends AppCompatActivity {
 
+    ImageView img_volver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_estadisticas);
+
+        img_volver=findViewById(R.id.img_voler);
+
+        Intent volver=new Intent(getApplicationContext(), Perfil_usuario.class);
+
+        img_volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(volver);
+            }
+        });
     }
 }
