35e06a94c3ef2cc06f5718df6b04e9b333e0290f
Alexander <alepues1@gmail.com>
2023-11-21 17:57:50 -0500

    modificacion sub en rojo contraseña visible y no visible y letreros de indicacion editext

--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/registro_usuario.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/registro_usuario.java
@@ -7,13 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Button;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -28,6 +29,7 @@ public class registro_usuario extends AppCompatActivity {
 
     EditText etNombres, etApellidos, etCedula, etCorreo, etContraseña, etValidacion;
 
+
     CheckBox check;
 
     @Override
@@ -37,12 +39,19 @@ public class registro_usuario extends AppCompatActivity {
 
         btnRegistrarse=findViewById(R.id.btn_registrarse);
 
-        etNombres = findViewById(R.id.etIngreNombre);
-        etApellidos = findViewById(R.id.etIngreApellidos);
-        etCedula = findViewById(R.id.etIngreID);
-        etCorreo = findViewById(R.id.etIngreCorreo);
-        etContraseña = findViewById(R.id.etIngreContraseña);
-        etValidacion = findViewById(R.id.etConfiContraseña);
+        TextInputLayout nombres = findViewById(R.id.etIngreNombre);
+        TextInputLayout apellidos = findViewById(R.id.etIngreApellidos);
+        TextInputLayout cedula = findViewById(R.id.etIngreID);
+        TextInputLayout correo = findViewById(R.id.etIngreCorreo);
+        TextInputLayout contraseña = findViewById(R.id.etIngreContraseña);
+        TextInputLayout validacion = findViewById(R.id.etConfiContraseña);
+
+        etNombres = nombres.getEditText();
+        etApellidos = apellidos.getEditText();
+        etCedula = cedula.getEditText();
+        etCorreo = correo.getEditText();
+        etContraseña = contraseña.getEditText();
+        etValidacion = validacion.getEditText();
 
         check = findViewById(R.id.check_terminos);
 
@@ -50,13 +59,14 @@ public class registro_usuario extends AppCompatActivity {
 
         btnRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                /*String nombres = etNombres.getText().toString();
+            public void onClick(View view) {/*
+                String nombres = etNombres.getText().toString();
                 String apellidos = etApellidos.getText().toString();
                 String cedula = etCedula.getText().toString();
                 String correo = etCorreo.getText().toString();
                 String contraseña = etContraseña.getText().toString();
-                String validacionContraseña = etValidacion.getText().toString();*/
+                String validacionContraseña = etValidacion.getText().toString();
+                */
 
                 /*if(check.isSelected() && nombres.isEmpty() || apellidos.isEmpty() || cedula.isEmpty() || correo.isEmpty() || contraseña.isEmpty() || validacionContraseña.isEmpty()  ) {
                     Toast.makeText(getBaseContext(), "Todos los campos deben llenarse", Toast.LENGTH_LONG).show();
@@ -105,35 +115,54 @@ public class registro_usuario extends AppCompatActivity {
         if (etNombres.getText().toString().isEmpty()){
             etNombres.setBackgroundColor(Color.RED);
             validarDate=false;
+        } else {
+            etNombres.setBackgroundColor(Color.TRANSPARENT);
         }
         if (etApellidos.getText().toString().isEmpty()){
             etApellidos.setBackgroundColor(Color.RED);
             validarDate=false;
+        }else {
+            etApellidos.setBackgroundColor(Color.TRANSPARENT);
         }
         if (etCedula.getText().toString().isEmpty()){
             etCedula.setBackgroundColor(Color.RED);
             validarDate=false;
+        }else {
+            etCedula.setBackgroundColor(Color.TRANSPARENT);
         }
         if (etCorreo.getText().toString().isEmpty()){
             etCorreo.setBackgroundColor(Color.RED);
             validarDate=false;
+        }else {
+            etCorreo.setBackgroundColor(Color.TRANSPARENT);
         }
         if (etContraseña.getText().toString().isEmpty()){
             etContraseña.setBackgroundColor(Color.RED);
             validarDate=false;
+        }else {
+            etContraseña.setBackgroundColor(Color.TRANSPARENT);
         }
         if (etValidacion.getText().toString().isEmpty()){
             etValidacion.setBackgroundColor(Color.RED);
             validarDate=false;
+        }else {
+            etValidacion.setBackgroundColor(Color.TRANSPARENT);
         }
         if (!etContraseña.getText().toString().equals(etValidacion.getText().toString())){
             etContraseña.setBackgroundColor(Color.RED);
             etValidacion.setBackgroundColor(Color.RED);
             validarDate=false;
+            Toast.makeText(getApplicationContext(), "La contraseña no coincide", Toast.LENGTH_LONG).show();
+        }else {
+            etContraseña.setBackgroundColor(Color.TRANSPARENT);
+            etValidacion.setBackgroundColor(Color.TRANSPARENT);
         }
+
         if (!check.isChecked()){
             check.setBackgroundColor(Color.RED);
             validarDate=false;
+        } else {
+            check.setBackgroundColor(Color.TRANSPARENT);
         }
 
         return validarDate;
@@ -167,10 +196,10 @@ public class registro_usuario extends AppCompatActivity {
             BufferedWriter bufferedWriter=new BufferedWriter(writer);
             bufferedWriter.write(
                     user.getEtNombres()+","+
-                        user.getEtApellidos()+","+
-                        user.getEtCedula()+","+
-                        user.getEtCorreo()+","+
-                        user.getEtContraseña()
+                            user.getEtApellidos()+","+
+                            user.getEtCedula()+","+
+                            user.getEtCorreo()+","+
+                            user.getEtContraseña()
             );
             bufferedWriter.newLine();
             bufferedWriter.close();
@@ -180,10 +209,4 @@ public class registro_usuario extends AppCompatActivity {
         }
 
     }
-
-
-
-
-
-
 }
