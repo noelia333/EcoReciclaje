b1e91517a66ae9e5f7ac81f1d3a1409e651c6f49
Cat3rine123 <caterine841210@gmail.com>
2023-11-25 20:46:14 -0500

    pantalla consejos lista

--- app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/pantalla_consejos.java
+++ app/src/main/java/co/edu/unipiloto/ecoreciclaje_equipo05/pantalla_consejos.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class pantalla_consejos extends AppCompatActivity {
 
     ImageView ivDevolver;
+
+
+    private TextView tvinformacion;
+    private Button btnConsejo1, btnConsejo2, btnConsejo3, btnConsejo4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +24,15 @@ public class pantalla_consejos extends AppCompatActivity {
         setContentView(R.layout.activity_pantalla_consejos);
 
         ivDevolver=findViewById(R.id.ivDevolver);
+
+        tvinformacion = findViewById(R.id.tvinformacion);
+        btnConsejo1 = findViewById(R.id.btn_consejo1);
+        btnConsejo2 = findViewById(R.id.btn_consejo2);
+        btnConsejo3 = findViewById(R.id.btn_consejo3);
+        btnConsejo4 = findViewById(R.id.btn_consejo4);
+
         ivDevolver.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent pantalla_consejos=new Intent(getApplicationContext(),Perfil_usuario.class);
@@ -25,6 +40,39 @@ public class pantalla_consejos extends AppCompatActivity {
             }
         });
 
+        btnConsejo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarInformacion("coloca los residuos reciclables separados de los que son organicos");
+            }
+        });
 
+        btnConsejo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarInformacion("procura reducir (dentro de lo posible) la cantidad de residuos que generas en casa o Reutiliza");
+            }
+        });
+
+        btnConsejo3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarInformacion("Deposita los residuos en el contenedor correspondiente" +
+                        "esta acción facilita que los residuos se incorporen antes a la cadena de reciclaje");
+            }
+        });
+
+        btnConsejo4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarInformacion("Reduce el tamaño de botellas y bricks" +
+                        "Un truco para optimizar nuestros espacios de almacenaje es «aplastar» los bricks y las botellas de plástico");
+            }
+        });
+    }
+    private void mostrarInformacion(String texto) {
+        tvinformacion.setText(texto);
     }
 }
+
+
